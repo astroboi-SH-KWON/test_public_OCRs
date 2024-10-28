8c217d49aae44ed2f480251fb6ce05d1012ef5f8
astroboi_m2 <terry007x@onycom.com>
2024-10-28 16:17:38 +0900

    Added gpu_flag for easyocr

--- test_ocr.py
+++ test_ocr.py
@@ -6,7 +6,7 @@ import time
 import glob
 
 
-def get_ocr_result_by_API(api_url, img_path, which_ocr='pytesseract', is_hexadecimal=True):
+def get_ocr_result_by_API(api_url, img_path, gpu, which_ocr='pytesseract', is_hexadecimal=True):
     if is_hexadecimal:
         print(f"\nis_hexadecimal {'|' * 30}\n")
         ocr_obj = image_to_bts(img_path)
@@ -15,6 +15,7 @@ def get_ocr_result_by_API(api_url, img_path, which_ocr='pytesseract', is_hexadec
         response = requests.post(api_url, json={
             'ocr_object': ocr_obj.hex(),
             'ocr_model': which_ocr.lower(),
+            'gpu_flag': str(gpu),
         })
     else:
         print(f"\nis_Base64 {'|' * 30}\n")
@@ -24,6 +25,7 @@ def get_ocr_result_by_API(api_url, img_path, which_ocr='pytesseract', is_hexadec
         response = requests.post(api_url, json={
             'ocr_object': base64.b64encode(ocr_obj.read()).decode(),
             'ocr_model': which_ocr.lower(),
+            'gpu_flag': str(gpu),
         })
 
         ocr_obj.close()
@@ -34,16 +36,20 @@ def get_ocr_result_by_API(api_url, img_path, which_ocr='pytesseract', is_hexadec
 
 
 if __name__ == "__main__":
-    url = "http://" + str(config.HOME_URL) + ":" + str(config.HOME_PORT) + "/test_publicOCRs_api"
+    # url = "http://" + str(config.HOME_URL) + ":" + str(config.HOME_PORT) + "/test_publicOCRs_api"
+    url = "http://192.168.3.138:" + str(config.HOME_PORT) + "/test_publicOCRs_api"
+    gpu = False
+    if '192.168.3.138' in url:
+        gpu = True
 
     img_path_list = glob.glob("./tests/data/*.jpg")
     for img_path in img_path_list:
         print(img_path.split("/")[-1])
-        which_ocr = 'tesseract_pub'
+        which_ocr = 'tesseract_ony'  # tesseract_ony, tesseract_pub, tesseract
         print(f"{which_ocr}>>>")
-        print(f"[{get_ocr_result_by_API(url, img_path, which_ocr=which_ocr)}]")
+        print(f"[{get_ocr_result_by_API(url, img_path, gpu, which_ocr=which_ocr, is_hexadecimal=True)}]")
 
         which_ocr = 'easyocr'
         print(f"{which_ocr}>>>")
-        print(get_ocr_result_by_API(url, img_path, which_ocr=which_ocr))
+        print(get_ocr_result_by_API(url, img_path, gpu, which_ocr=which_ocr, is_hexadecimal=True))
         print(f"\n\n")
